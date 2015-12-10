fa452caf758cee4c75360c47b1b28a478e54498a
orome656 <anthony.maladot@gmail.com>
2015-12-10 23:49:49 +0100

    Init actus Parser

---
+++ src/HOFCServerParser/Parsers/ActusParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace HOFCServerParser.Parsers
+{
+    public class ActusParser : Parser
+    {
+
+        protected override IEnumerable<HtmlNode> GetLines()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IModel ParseLine(HtmlNode line)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SaveToBDD()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
