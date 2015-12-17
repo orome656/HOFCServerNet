e89a9405de75c63083b76e9d48da21881697fc3b
orome656 <anthony.maladot@gmail.com>
2015-12-17 21:06:47 +0100

    Notification Client Controller

--- src/HOFCServerNet/Controllers/NotificationController.cs
+++ src/HOFCServerNet/Controllers/NotificationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using HOFCServerNet.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,8 +14,24 @@ namespace HOFCServerNet.Controllers
     {
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string notification_id, [FromBody]string uuid)
+        public void Post(string uuid, string notification_id)
         {
+            if(uuid != null && notification_id != null)
+            {
+                using (var bddContext = new BddContext())
+                {
+                    var notificationClient = new NotificationClient
+                    {
+                        UUID = uuid,
+                        NotificationID = notification_id
+                    };
+                    bddContext.NotificationClients.Add(notificationClient);
+                    bddContext.SaveChanges();
+                }
+            } else
+            {
+                // TODO add logs here
+            }
         }
     }
 }
