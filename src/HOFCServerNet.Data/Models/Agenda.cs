348d1920830e470a91d7d044e48dea3b80fb38cd
orome656 <anthony.maladot@gmail.com>
2015-12-20 22:13:13 +0100

    Add Agenda Model

---
+++ src/HOFCServerNet.Data/Models/Agenda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HOFCServerNet.Data.Models
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string Equipe1 { get; set; }
+        public string Equipe2 { get; set; }
+        public int? Score1 { get; set; }
+        public int? Score2 { get; set; }
+        public string Categorie { get; set; }
+        public string Semaine { get; set; }
+        public string InfosId { get; set; }
+    }
+}
