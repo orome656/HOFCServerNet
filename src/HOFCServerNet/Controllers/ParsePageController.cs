a5d4183419609b4b1323d127ec9100963305cf87
Anthony Maladot <anthony.maladot@gmail.com>
2016-01-08 10:15:43 +0100

    Add diaporama parser

---
+++ src/HOFCServerNet/Controllers/ParsePageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using HOFCServerNet.Parsers;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HOFCServerNet.Controllers
+{
+    [Route("api/[controller]")]
+    public class ParsePageController : Controller
+    {
+        // GET api/values/{url}
+        [HttpGet("{url}")]
+        public List<string> Get(string url)
+        {
+            if(url.Contains("en-image"))
+            {
+                return DiaporamaParser.Parse(url);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/HOFCServerNet/Parsers/DiaporamaParser.cs
@@ -0,0 +1,40 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HOFCServerNet.Parsers
+{
+    public class DiaporamaParser
+    {
+        public static List<String> Parse(string url)
+        {
+            IEnumerable<HtmlNode> listNode = DiaporamaParser.GetLines(url);
+            List<String> list = DiaporamaParser.ParseLine(listNode);
+            return list;
+        }
+
+        private static IEnumerable<HtmlNode> GetLines(string url)
+        {
+            var httpClient = new HttpClient();
+            string html = httpClient.GetStringAsync(url).Result;
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+            var root = document.DocumentNode;
+            var lines = root.SelectSingleNode("//div[@class='ngg-galleryoverview ngg-ajax-pagination-none']").ChildNodes.Where(item => item.GetAttributeValue("class", "").ToString().Equals("ngg-gallery-thumbnail-box"));
+            return lines;
+        }
+
+        private static List<string> ParseLine(IEnumerable<HtmlNode> listNode)
+        {
+            List<string> urls = new List<string>();
+            foreach(HtmlNode line in listNode)
+            {
+                urls.Add(line.ChildNodes.ElementAt(1).ChildNodes.ElementAt(1).GetAttributeValue("href", null));
+            }
+            return urls;
+        }
+    }
+}
