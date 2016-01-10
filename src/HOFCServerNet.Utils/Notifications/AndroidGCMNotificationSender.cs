6e708d5e74d0c8dba97c8d77f9aac29a2b0e7810
Anthony Maladot <anthony.maladot@gmail.com>
2016-01-10 16:15:50 +0100

    Notification sender correction

--- src/HOFCServerNet.Utils/Notifications/AndroidGCMNotificationSender.cs
+++ src/HOFCServerNet.Utils/Notifications/AndroidGCMNotificationSender.cs
@@ -17,7 +17,6 @@ namespace HOFCServerParser.Utils
             using (var bddContext = new BddContext())
             {
                 string SERVER_API_KEY = System.Environment.GetEnvironmentVariable("SERVER_API_KEY");
-                var SENDER_ID = System.Environment.GetEnvironmentVariable("SENDER_ID");
                 var value = message;
                 HttpClient client = new HttpClient();
 
@@ -25,16 +24,15 @@ namespace HOFCServerParser.Utils
                 client.DefaultRequestHeaders
                       .Accept
                       .Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
-                client.DefaultRequestHeaders.Add("Authorization", string.Format("key={0}", SERVER_API_KEY));
-                client.DefaultRequestHeaders.Add("Sender", string.Format("id={0}", SENDER_ID));
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", string.Format("key={0}", SERVER_API_KEY));
                 foreach(NotificationClient nClient in bddContext.NotificationClients.ToList())
                 {
                     string deviceId = nClient.NotificationID;
-                    string postData = "collapse_key=score_update&time_to_live=108&delay_while_idle=1&data.message=" + value + "&data.time=" + System.DateTime.Now.ToString() + "&registration_id=" + deviceId + "";
+                    string postData = "collapse_key=score_update&time_to_live=108&delay_while_idle=1&data.title=" + titre + "&data.message=" + value + "&data.time=" + System.DateTime.Now.ToString() + "&registration_id=" + deviceId + "";
 
                     var content = new StringContent(postData,
                                                         Encoding.UTF8,
-                                                        "application/x-www-form-urlencoded;charset=UTF-8");
+                                                        "application/x-www-form-urlencoded");
 
                     HttpResponseMessage response = client.PostAsync(URL, content).Result;
                 }
