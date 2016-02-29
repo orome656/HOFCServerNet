3935848c242e40ae58622d1e45d6284d487a41eb
orome656 <anthony.maladot@gmail.com>
2016-02-29 16:48:50 +0100

    Configure Grunt/Bower/NPM for frontend development

---
+++ src/HOFCServerNet/Scripts/Controllers/CalendrierController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('HOFCApp')
+        .controller('CalendrierController', CalendrierController);
+
+    CalendrierController.$inject = ['$scope', 'matchsServices'];
+
+    function CalendrierController($scope, matchsServices) {
+        $scope.matchs = matchsServices.query();
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'CalendrierController';
+
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ src/HOFCServerNet/Scripts/Services/matchsServices.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('HOFCApp')
+        .factory('matchsServices', ['$resource', function ($resource) {
+            return $resource('/api/Match', {}, {
+                query: { method: 'GET', params: {}, isArray: true }
+            });
+        }]);
+
+})();
