b76e3a498a0f00d20786cff98b5d7e6272c66ddb
orome656 <anthony.maladot@gmail.com>
2016-03-02 17:15:24 +0100

    Calendrier Controller edit

--- src/HOFCServerNet/Scripts/Controllers/CalendrierController.js
+++ src/HOFCServerNet/Scripts/Controllers/CalendrierController.js
@@ -5,16 +5,31 @@
         .module('HOFCApp')
         .controller('CalendrierController', CalendrierController);
 
-    CalendrierController.$inject = ['$scope', 'matchsServices'];
+    CalendrierController.$inject = ['$scope', '$filter', '$routeParams', '$location', 'matchsServices'];
 
-    function CalendrierController($scope, matchsServices) {
-        $scope.matchs = matchsServices.query();
-        /* jshint validthis:true */
-        var vm = this;
-        vm.title = 'CalendrierController';
+    function CalendrierController($scope, $filter, $routeParams, $location, matchsServices) {
+        $scope.categorie = $routeParams.categorie;
+        $scope.matchs = null;
+        matchsServices.query(function (result) {
+            $scope.matchs = $filter('CalendrierFilter')(result, $routeParams.categorie);
+        });
 
-        activate();
-
-        function activate() { }
+        $scope.selectedIndex = parseInt($routeParams.categorie[$routeParams.categorie.length - 1]) -1;
+        	
+        $scope.tabs = [{
+            title: 'Equipe 1',
+            categorie: 'equipe1'
+        },{
+    		title: 'Equipe 2',
+    		categorie: 'equipe2'
+    	},{
+        	title: 'Equipe 3',
+        	categorie: 'equipe3'
+        }]
+        	
+        $scope.onTabSelected = function(tab) {
+            $location.url('/calendriers/' + tab.categorie);
+        };
+        
     }
 })();
