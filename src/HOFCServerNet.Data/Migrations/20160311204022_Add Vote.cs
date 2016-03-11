297a0d95cf458d39b1e5b51160186e9b5a63d3d6
Anthony Maladot <anthony.maladot@gmail.com>
2016-03-11 21:43:24 +0100

    Add database migration for vote

---
+++ src/HOFCServerNet.Data/Migrations/20160311204022_Add Vote.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+
+namespace HOFCServerNet.Migrations
+{
+    public partial class AddVote : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_JoueurPoste_Joueur_IdJoueur", table: "JoueurPoste");
+            migrationBuilder.DropForeignKey(name: "FK_JoueurPoste_Poste_IdPoste", table: "JoueurPoste");
+            migrationBuilder.AddForeignKey(
+                name: "FK_JoueurPoste_Joueur_IdJoueur",
+                table: "JoueurPoste",
+                column: "IdJoueur",
+                principalTable: "Joueur",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_JoueurPoste_Poste_IdPoste",
+                table: "JoueurPoste",
+                column: "IdPoste",
+                principalTable: "Poste",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_JoueurPoste_Joueur_IdJoueur", table: "JoueurPoste");
+            migrationBuilder.DropForeignKey(name: "FK_JoueurPoste_Poste_IdPoste", table: "JoueurPoste");
+            migrationBuilder.AddForeignKey(
+                name: "FK_JoueurPoste_Joueur_IdJoueur",
+                table: "JoueurPoste",
+                column: "IdJoueur",
+                principalTable: "Joueur",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_JoueurPoste_Poste_IdPoste",
+                table: "JoueurPoste",
+                column: "IdPoste",
+                principalTable: "Poste",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
