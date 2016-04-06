01b455500dd16152ee120b2fb2b23e4a5de53d88
orome656 <anthony.maladot@gmail.com>
2016-04-06 19:07:21 +0200

    Helper for active navbar item

---
+++ src/HOFCServerNet/Helpers/HtmlHelpers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HOFCServerNet.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static string IsActive(
+            this IHtmlHelper html,
+            string actionName,
+            string controllerName)
+        {
+            var routeData = html.ViewContext.RouteData;
+
+            var routeAction = (string)routeData.Values["action"];
+            var routeControl = (string)routeData.Values["controller"];
+
+            var returnActive = controllerName == routeControl;
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
