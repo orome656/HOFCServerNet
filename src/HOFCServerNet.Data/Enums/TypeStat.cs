d04cfe26bb77529c55b548a5ed117b772139add3
orome656 <anthony.maladot@gmail.com>
2016-04-09 14:18:13 +0200

    Add Stats table and enum

---
+++ src/HOFCServerNet.Data/Enums/TypeStat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HOFCServerNet.Data.Enums
+{
+    public enum TypeStat
+    {
+        BUT = 0,
+        PASSE = 1,
+        CARTON_JAUNE = 2,
+        CARTON_ROUGE = 3
+    }
+}
---
+++ src/HOFCServerNet.Data/Models/Stat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HOFCServerNet.Data.Models
+{
+    public class Stat
+    {
+        public int Id { get; set; }
+        public Joueur Joueur { get; set; }
+        public int TypeStat { get; set; } // But, Passe ...
+        public int Nombre { get; set; }
+    }
+}
