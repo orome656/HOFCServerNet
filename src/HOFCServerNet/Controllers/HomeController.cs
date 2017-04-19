392ec067fd692135a52cacd2c4941aad10ff2e4d
orome656 <anthony.maladot@gmail.com>
2017-04-19 18:47:23 +0200

    Re add auth page

--- src/HOFCServerNet/Controllers/HomeController.cs
+++ src/HOFCServerNet/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TestSPA.Controllers
+namespace HOFCServerNet.Controllers
 {
     public class HomeController : Controller
     {
---
+++ src/HOFCServerNet/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HOFCServerNet.Data.Models;
+using System.Security.Claims;
+using HOFCServerNet.ViewModels.Profile;
+using Microsoft.AspNetCore.Identity;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HOFCServerNet.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public ProfileController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var user = await GetCurrentUserAsync();
+            IndexViewModel viewModel = new IndexViewModel();
+            viewModel.Login = user.UserName;
+            return View(viewModel);
+        }
+
+        private async Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return await _userManager.FindByIdAsync(_userManager.GetUserId(User));
+        }
+    }
+}
