45b515b0c62170ff2845309a05cc852bfe2d6a53
orome656 <anthony.maladot@gmail.com>
2017-06-05 17:40:10 +0200

    Add foreign key id

--- src/HOFCServerNet.Data/Models/Composition.cs
+++ src/HOFCServerNet.Data/Models/Composition.cs
@@ -8,8 +8,11 @@ namespace HOFCServerNet.Data.Models
     public class Composition
     {
         public int Id { get; set; }
+        public int JoueurId { get; set; }
         public Joueur Joueur { get; set; }
+        public int PosteId { get; set; }
         public Poste Poste { get; set; }
+        public int MatchId { get; set; }
         public Match Match { get; set; }
     }
 }
